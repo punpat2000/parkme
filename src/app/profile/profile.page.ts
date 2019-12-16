327051472377ab1ff23026b7922c2a190172f57c
BankPitawat <51756741+BankPitawat@users.noreply.github.com>
2019-12-16 23:53:58 +0700

    1.2.4
    Some profile changes

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsermgmtService } from '../usermgmt.service'
+import { ProfiledbService } from '../profiledb.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,15 +8,23 @@ import { UsermgmtService } from '../usermgmt.service'
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  name: String
+  change: Boolean
 
   constructor(
-    private userm: UsermgmtService
+    private userm: UsermgmtService,
+    private profile: ProfiledbService,
   ) { }
 
   ngOnInit() {
+    this.name = this.profile.getDisplayName()
   }
 
   logout() {
     this.userm.logout();
+  }
+
+  save() {
+    
   }
 }
