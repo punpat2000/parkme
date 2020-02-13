2f98cac0dabd2fb0183a8b5a01679200c8fea6e4
punpat2000 <saint12302@gmail.com>
2020-02-14 05:57:05 +0700

    minor

--- src/app/services/profiledb.service.ts
+++ src/app/services/profiledb.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from '../../models/user.model';
-import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AlertController } from '@ionic/angular'
 
@@ -11,7 +10,6 @@ import { AlertController } from '@ionic/angular'
 })
 export class ProfiledbService {
 
-  //profile$: Observable<User>;
   private userId: string;
   name: string;
 
