f6285edc5b99dc16e0daefe2a87df0454f16a219
punpat2000 <saint12302@gmail.com>
2020-04-06 12:01:42 +0700

    minor change

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -56,7 +56,7 @@ export class UserService implements OnDestroy {
       username: user.email,
       uid: user.uid,
       name: user.displayName,
-      url: user.photoURL
+      url: user.photoURL ?? 'https://firebasestorage.googleapis.com/v0/b/parkmebysaint.appspot.com/o/blank-profile.png?alt=media&token=4f775ff6-4520-4ecf-b2e0-04148fedaaa7',
     }
     return data;
   }
