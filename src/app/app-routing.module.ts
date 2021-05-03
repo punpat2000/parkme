eca3b00e5e4cf1ac5201324fcca8527ef226e315
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-05-03 12:56:51 +0700

    fix anonymous route handling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -38,6 +38,10 @@ const routes: Routes = [
 		canActivate: [AngularFireAuthGuard],
 		data: { authGuardPipe: redirectUnauthorizedToLogin },
 	},
+	{
+		path: '**',
+		redirectTo: 'tab',
+	},
 ];
 
 @NgModule({
