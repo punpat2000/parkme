c1130c8e636397e77a6b72bb6e11fa37ce4c78d3
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-05-03 12:08:46 +0700

    format and change syntax for new firebase version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,49 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo, canActivate } from '@angular/fire/auth-guard'
+import {
+	AngularFireAuthGuard,
+	redirectUnauthorizedTo,
+	redirectLoggedInTo,
+} from '@angular/fire/auth-guard';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
 const redirectLoggedInToHome = () => redirectLoggedInTo(['tab']);
 
 const routes: Routes = [
-  { 
-    path: '', 
-    redirectTo: 'login', 
-    pathMatch: 'full' 
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectLoggedInToHome }
-  },
-  {
-    path: 'terms',
-    loadChildren: () => import('./terms/terms.module').then( m => m.TermsPageModule)
-  },
-  {
-    path: 'privacy',
-    loadChildren: () => import('./privacy/privacy.module').then( m => m.PrivacyPageModule)
-  },
-  {
-    path: 'tab',
-    loadChildren: () => import('./tab/tab.module').then( m => m.TabPageModule),
-    canActivate: [AngularFireAuthGuard],
-    data: { authGuardPipe: redirectUnauthorizedToLogin }
-  },
-
+	{
+		path: '',
+		redirectTo: 'login',
+		pathMatch: 'full',
+	},
+	{
+		path: 'login',
+		loadChildren: () =>
+			import('./login/login.module').then((m) => m.LoginPageModule),
+		canActivate: [AngularFireAuthGuard],
+		data: { authGuardPipe: redirectLoggedInToHome },
+	},
+	{
+		path: 'terms',
+		loadChildren: () =>
+			import('./terms/terms.module').then((m) => m.TermsPageModule),
+	},
+	{
+		path: 'privacy',
+		loadChildren: () =>
+			import('./privacy/privacy.module').then((m) => m.PrivacyPageModule),
+	},
+	{
+		path: 'tab',
+		loadChildren: () => import('./tab/tab.module').then((m) => m.TabPageModule),
+		canActivate: [AngularFireAuthGuard],
+		data: { authGuardPipe: redirectUnauthorizedToLogin },
+	},
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
-  exports: [RouterModule]
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+	],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import {
+	AngularFirestore,
+	AngularFirestoreDocument,
+} from '@angular/fire/firestore';
 import { User } from '../models';
 import { tap, pluck } from 'rxjs/operators';
-import { AlertController } from '@ionic/angular'
+import { AlertController } from '@ionic/angular';
 import { Observable, of, ReplaySubject } from 'rxjs';
 import { switchMap, take, shareReplay, filter } from 'rxjs/operators';
 import { untilDestroyed } from 'ngx-take-until-destroy';
@@ -12,125 +15,138 @@ import { Router } from '@angular/router';
 import DEFAULT_IMG_URL from './default-img-url';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class UserService implements OnDestroy {
+	private userId: string;
+	public name: string;
+	private user$: Observable<User>;
 
-  private userId: string;
-  public name: string;
-  private user$: Observable<User>;
+	constructor(
+		private afAuth: AngularFireAuth,
+		private db: AngularFirestore,
+		private router: Router,
+		private alertController: AlertController
+	) {
+		this.userInitialize();
+	}
 
-  constructor(
-    private afAuth: AngularFireAuth,
-    private db: AngularFirestore,
-    private router: Router,
-    private alertController: AlertController
-  ) {
-    this.userInitialize();
-  }
+	private userInitialize(): void {
+		this.user$ = this.afAuth.authState.pipe<
+			firebase.User,
+			firebase.User,
+			User,
+			User
+		>(
+			untilDestroyed(this),
+			tap((user) => (this.userId = user ? user.uid : null)),
+			switchMap((user) =>
+				user
+					? this.db.doc<User>(`profiles/${user.uid}`).valueChanges()
+					: of(null)
+			),
+			shareReplay(1)
+		);
+		const onAuthStateChanged$: ReplaySubject<firebase.User> = new ReplaySubject<firebase.User>(
+			1
+		);
+		this.afAuth.onAuthStateChanged(onAuthStateChanged$);
+		onAuthStateChanged$.pipe(filter((user) => !_.isNil(user))).subscribe({
+			next: this._checkUserDoc.bind(this),
+			error: console.log,
+		});
+	}
 
-  private userInitialize(): void {
-    this.user$ = this.afAuth.authState.pipe<firebase.User, firebase.User, User, User>(
-      untilDestroyed(this),
-      tap(user => this.userId = user ? user.uid : null),
-      switchMap(user => user ? this.db.doc<User>(`profiles/${user.uid}`).valueChanges() : of(null)),
-      shareReplay(1),
-    );
-    const onAuthStateChanged$: ReplaySubject<firebase.User> = new ReplaySubject<firebase.User>(1);
-    this.afAuth.auth.onAuthStateChanged(onAuthStateChanged$);
-    onAuthStateChanged$
-      .pipe(filter(user => !_.isNil(user)))
-      .subscribe({
-        next: this._checkUserDoc.bind(this),
-        error: console.log
-      });
-  }
+	private _checkUserDoc(user: firebase.User): void {
+		this.userId = user.uid;
+		const userRef: AngularFirestoreDocument<User> = this.db.doc(
+			`profiles/${user.uid}`
+		);
+		userRef.ref.get().then((doc) => {
+			if (doc.exists) return;
+			const data: User = this.userDefault(user);
+			userRef.set(data, { merge: true });
+		});
+	}
 
-  private _checkUserDoc(user: firebase.User): void {
-    this.userId = user.uid;
-    const userRef: AngularFirestoreDocument<User> = this.db.doc(`profiles/${user.uid}`);
-    userRef.ref.get().then(doc => {
-      if (doc.exists) return;
-      const data: User = this.userDefault(user);
-      userRef.set(data, { merge: true });
-    })
-  }
+	private userDefault({
+		email,
+		uid,
+		displayName,
+		photoURL,
+	}: firebase.User): User {
+		const data: User = {
+			username: email,
+			uid: uid,
+			name: displayName,
+			url: photoURL ?? DEFAULT_IMG_URL,
+		};
+		return data;
+	}
 
-  private userDefault({email, uid, displayName, photoURL}: firebase.User): User {
-    const data: User = {
-      username: email,
-      uid: uid,
-      name: displayName,
-      url: photoURL ?? DEFAULT_IMG_URL,
-    }
-    return data;
-  }
+	ngOnDestroy() {}
 
-  ngOnDestroy() { }
+	async showAlert(header: string, message: string) {
+		const alert = await this.alertController.create({
+			header: header,
+			message: message,
+			buttons: ['Ok'],
+		});
+		alert.present();
+		return await alert.onDidDismiss();
+	}
 
+	async updateProfile(
+		name: string,
+		phonenumber: string,
+		url: string
+	): Promise<void> {
+		if (name.length <= 0) {
+			this.showAlert('Oh no!', 'Please enter name');
+			console.log('error');
+			return;
+		}
+		(await this.afAuth.currentUser).updateProfile({ photoURL: url });
+		await this.userId$
+			.pipe(take(1))
+			.toPromise()
+			.then((uid) => {
+				if (uid) {
+					this.db.doc(`profiles/${uid}`).update({
+						displayName: name,
+						phonenumber: phonenumber ?? null,
+						url: url,
+					});
+				}
+			})
+			.catch(console.log);
+		this.showAlert('Done!', 'Your profile has been updated');
+		console.log('Profile updated');
+	}
 
-  async showAlert(header: string, message: string) {
-    const alert = await this.alertController.create({
-      header: header,
-      message: message,
-      buttons: ['Ok']
-    });
-    alert.present();
-    return await alert.onDidDismiss();
-  }
+	get uid(): string {
+		return this.userId;
+	}
 
+	get userId$(): Observable<string> {
+		return this.profile.pipe(pluck('uid'));
+	}
 
-  async updateProfile(name: string, phonenumber: string, url: string): Promise<void> {
-    if (name.length <= 0) {
-      this.showAlert('Oh no!', 'Please enter name');
-      console.log('error');
-      return;
-    }
-    this.afAuth.auth.currentUser.updateProfile({ photoURL: url });
-    await this.userId$
-      .pipe(take(1))
-      .toPromise()
-      .then(uid => {
-        if (uid) {
-          this.db.doc(`profiles/${uid}`).update({
-            displayName: name,
-            phonenumber: phonenumber ?? null,
-            url: url,
-          })
-        };
-      })
-      .catch(console.log);
-    this.showAlert('Done!', 'Your profile has been updated');
-    console.log('Profile updated');
-  }
+	get profile(): Observable<User> {
+		return this.user$;
+	}
 
-  get uid(): string {
-    return this.userId;
-  }
-
-  get userId$(): Observable<string> {
-    return this.profile.pipe(pluck('uid'));
-  }
-
-  get profile(): Observable<User> {
-    return this.user$;
-  }
-
-  setNotHost(): void {
-    this.userId$
-      .pipe(take(1))
-      .subscribe(uid => {
-      this.db.doc(`profiles/${uid}`).update({ host: false }).catch(console.log);
-    });
-  }
-
-  async logout(): Promise<void> {
-    try {
-      await this.afAuth.auth.signOut();
-      this.router.navigate(['login']);
-    } catch (e) {
-      throw new Error();
-    }
-  }
-
+	setNotHost(): void {
+		this.userId$.pipe(take(1)).subscribe((uid) => {
+			this.db.doc(`profiles/${uid}`).update({ host: false }).catch(console.log);
+		});
+	}
+	async logout(): Promise<void> {
+		try {
+			await this.afAuth.signOut();
+			this.router.navigate(['login']);
+		} catch (e) {
+			throw new Error();
+		}
+	}
 }
